57bdda512ec8eee90974fc645a557bf85fe5bfc6
Tiffany Ho <tiaho@ucdavis.edu>
2015-04-25 10:54:35 -0700

    now transforming the transcriptome_fitted_vals file in a separate script

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(shiny)
 library(ggplot2)
 library(reshape2)
 
-counts <-read.csv("data/transcriptome_fitted_vals_22Jun11.csv", as.is=T)
+counts <-read.csv("data/transformed_transcriptome_fitted_vals.csv", as.is=T)
 pvalues <- read.delim("data/transcriptome_annotated_20Jun11.tsv", as.is=T)
 
 shinyServer(function(input, output) {
@@ -12,30 +12,6 @@ shinyServer(function(input, output) {
     
   })
 })
-
-# gets data into format: gene, expn, species, type
-gene <- vector()
-expn <- vector()
-species <- vector()
-type <- vector()
-
-for (i in 2:13){
-  tmp_name <- names(counts)[i]
-  split_name <- strsplit(tmp_name, "[.]")
-  
-  for (j in 1:length(counts$X)){
-    pos = (i - 2) * length(counts$X) + j
-    gene[pos] <- counts$X[j]
-    expn[pos] <- counts[, i][j]
-    species[pos] <- split_name[[1]][1]
-    type[pos] <- split_name[[1]][2]
-  }
-  
-}
-  
-data <- cbind(gene, expn, species, type)
-data <- data.frame(gene, expn, species, type, row.names = NULL)
-
 
 
 
---
+++ transform_transcriptome_fitted_vals.R
@@ -0,0 +1,28 @@
+counts <-read.csv("data/transcriptome_fitted_vals_22Jun11.csv", as.is=T)
+
+# gets data into format: gene, expn, species, type
+gene <- vector()
+expn <- vector()
+species <- vector()
+type <- vector()
+
+for (i in 2:13){ # loops through each column in the orignal file
+  tmp_name <- names(counts)[i]
+  split_name <- strsplit(tmp_name, "[.]")
+  
+  for (j in 1:length(counts$X)){ # loops through each row in the orignal file
+    pos = (i - 2) * length(counts$X) + j
+    gene[pos] <- counts$X[j]
+    expn[pos] <- counts[, i][j]
+    species[pos] <- split_name[[1]][1]
+    type[pos] <- split_name[[1]][2]
+  }
+  
+}
+
+#  combines the individual vectorss
+data <- cbind(gene, expn, species, type)
+data <- data.frame(gene, expn, species, type, row.names = NULL)
+
+# writes out the table
+write.table(data, file = "transformed_transcriptome_fitted_vals.csv", row.names = FALSE)
